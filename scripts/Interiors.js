91547d2975181a479bafe43f876c4218233e6f33
Tricia Swift <tricia.swift8@gmail.com>
2023-08-10 14:26:02 -0500

    added event listeners that change the transient state when selected

--- scripts/Interiors.js
+++ scripts/Interiors.js
@@ -1,19 +1,26 @@
+import { setInteriorId } from "./TransientState.js";
+
+const handleInteriorChoice = (changeEvent) => {
+  if (changeEvent.target.id === "interiors") {
+    const chosenOption = parseInt(changeEvent.target.value);
+    setInteriorId(chosenOption);
+  }
+};
+
 export const InteriorOptions = async () => {
   const response = await fetch("http://localhost:8088/interiors");
   const interiors = await response.json();
 
-  let interiorHTML = `
-    <div class="choices_interior">
-      <select id="interiors">
-          <option value="0">Select an interior material</option>`;
+  let interiorHTML = `<select id="interiors">`;
+  interiorHTML += `<option value="0">Select an interior material</option>`;
+
+  document.addEventListener("change", handleInteriorChoice);
 
   const interiorOptionArray = interiors.map((interior) => {
     return `<option value="${interior.id}">${interior.interior}</option>`;
   });
 
   interiorHTML += interiorOptionArray.join("");
-  interiorHTML += `
-      </select>
-    </div>`;
+  interiorHTML += `</select>`;
   return interiorHTML;
 };
--- scripts/Wheels.js
+++ scripts/Wheels.js
@@ -1,19 +1,26 @@
+import { setWheelId } from "./TransientState.js";
+
+const handleWheelChoice = (changeEvent) => {
+  if (changeEvent.target.id === "wheels") {
+    const chosenOption = parseInt(changeEvent.target.value);
+    setWheelId(chosenOption);
+  }
+};
+
 export const WheelsOptions = async () => {
   const response = await fetch("http://localhost:8088/wheels");
   const wheels = await response.json();
 
-  let wheelHTML = `
-        <div class="choices_wheel">
-          <select id="wheels">
-              <option value="0">Select a wheel style</option>`;
+  let wheelHTML = `<select id="wheels">`;
+  wheelHTML += `<option value="0">Select a wheel style</option>`;
+
+  document.addEventListener("change", handleWheelChoice);
 
   const wheelOptionArray = wheels.map((wheel) => {
     return `<option value="${wheel.id}">${wheel.wheel}</option>`;
   });
 
   wheelHTML += wheelOptionArray.join("");
-  wheelHTML += `
-          </select>
-        </div>`;
+  wheelHTML += `</select>`;
   return wheelHTML;
 };
