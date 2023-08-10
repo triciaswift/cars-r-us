f9baa78ea15bdd8cd5d6a685c268a0e065b755c4
Tricia Swift <tricia.swift8@gmail.com>
2023-08-10 17:49:20 -0500

    rendered all custom orders to the DOM & updated CSS

---
+++ scripts/OrderButton.js
@@ -0,0 +1,24 @@
+import { SaveOrder, currentTransientState } from "./TransientState.js";
+
+const handleButtonClick = (clickEvent) => {
+  if (clickEvent.target.id === "order_button") {
+    const transientState = currentTransientState();
+    if (
+      transientState.paintId &&
+      transientState.interiorId &&
+      transientState.techId &&
+      transientState.wheelId
+    ) {
+      SaveOrder();
+    } else {
+      const errorMessage = `<div class="message">Please select all 4 items.</div>`;
+      const parentTag = document.querySelector(".error_message");
+      parentTag.innerHTML = errorMessage;
+    }
+  }
+};
+
+export const OrderButton = () => {
+  document.addEventListener("click", handleButtonClick);
+  return `<button id="order_button">Place Car Order</button>`;
+};
--- scripts/Technologies.js
+++ scripts/Technologies.js
@@ -1,26 +1,26 @@
-import { technologyId } from "./TransientState.js";
+import { setTechId } from "./TransientState.js";
 
-const handleTechnologyChoice = (changeEvent) => {
-  if (changeEvent.target.id === "technologies") {
+const handleTechChoice = (changeEvent) => {
+  if (changeEvent.target.id === "techs") {
     const chosenOption = parseInt(changeEvent.target.value);
-    technologyId(chosenOption);
+    setTechId(chosenOption);
   }
 };
 
-export const TechnologiesOptions = async () => {
-  const response = await fetch("http://localhost:8088/technologies");
-  const technologies = await response.json();
+export const TechsOptions = async () => {
+  const response = await fetch("http://localhost:8088/techs");
+  const techs = await response.json();
 
-  let technologyHTML = `<select id="technologies">`;
-  technologyHTML += `<option value="0">Select a technology package</option>`;
+  let techHTML = `<select id="techs">`;
+  techHTML += `<option value="0">Select a technology package</option>`;
 
-  document.addEventListener("change", handleTechnologyChoice);
+  document.addEventListener("change", handleTechChoice);
 
-  const technologyOptionArray = technologies.map((technology) => {
-    return `<option value="${technology.id}">${technology.technology}</option>`;
+  const techOptionArray = techs.map((tech) => {
+    return `<option value="${tech.id}">${tech.tech}</option>`;
   });
 
-  technologyHTML += technologyOptionArray.join("");
-  technologyHTML += `</select>`;
-  return technologyHTML;
+  techHTML += techOptionArray.join("");
+  techHTML += `</select>`;
+  return techHTML;
 };
